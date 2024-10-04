c77b20f96092651149cc8c8a73e10c59bfa339f8
valentinolivero <valentinoliveroroma@gmail.com>
2024-10-03 23:12:49 -0300

    - Producer configurado en Node
    Se eliminó la clase Producer desde springboot para establecerlo en Node. Node -> Producer
    Springboot -> Consumer

--- backend-servidor/stockearte/src/main/java/com/unla/stockearte/config/KafkaConfig.java
+++ backend-servidor/stockearte/src/main/java/com/unla/stockearte/config/KafkaConfig.java
@@ -1,16 +1,12 @@
 package com.unla.stockearte.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,22 +15,8 @@ import java.util.Map;
 @EnableKafka
 public class KafkaConfig {
 
-    private String bootstrapServers = "localhost:9092"; // puerto configurado en el service.properties del kafka instalado en el equipo.
+    private String bootstrapServers = "localhost:9092"; // puerto de tu Kafka
 
-    @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
-    }
-
-    @Bean
-    public DefaultKafkaProducerFactory<String, String> producerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
-    }
-    
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> configProps = new HashMap<>();
