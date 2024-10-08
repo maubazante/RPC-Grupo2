313f1daf41a744f93feccf8662194185e7d07bd2
Sergio Robles <mpthrash@gmail.com>
2024-10-08 00:16:54 -0300

    Update ProductoService.java

--- backend-servidor/stockearte/src/main/java/com/unla/stockearte/service/ProductoService.java
+++ backend-servidor/stockearte/src/main/java/com/unla/stockearte/service/ProductoService.java
@@ -79,7 +79,7 @@ public class ProductoService extends ProductoServiceImplBase {
 	// ==========================
 
 	@Transactional(readOnly = false, rollbackForClassName = { "java.lang.Throwable",
-			"jav	a.lang.Exception" }, propagation = Propagation.REQUIRED)
+			"java.lang.Exception" }, propagation = Propagation.REQUIRED)
 	@Override
 	public void createProducto(CreateProductoRequest request, StreamObserver<CreateProductoResponse> responseObserver) {
 		
