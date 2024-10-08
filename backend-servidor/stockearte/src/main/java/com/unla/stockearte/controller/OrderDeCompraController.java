775548b0ff3b390cb9eabe5bd09ac7b61ce80a6a
Mauro Bazante <maurobazante3@gmail.com>
2024-10-08 12:01:39 -0300

    -- RequestBody

--- backend-servidor/stockearte/src/main/java/com/unla/stockearte/controller/OrderDeCompraController.java
+++ backend-servidor/stockearte/src/main/java/com/unla/stockearte/controller/OrderDeCompraController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +26,7 @@ public class OrderDeCompraController {
 	OrdenDeCompraService orderDeCompraService;
 
 	@PostMapping
-	public ResponseEntity<OrdenDeCompra> addOrdenDeCompra(OrdenCompraRequest request) {
+	public ResponseEntity<OrdenDeCompra> addOrdenDeCompra(@RequestBody OrdenCompraRequest request) {
 		OrdenDeCompra oc = orderDeCompraService.crearOrdenCompra(request);
 		return new ResponseEntity<OrdenDeCompra>(oc, HttpStatus.CREATED);
 	}
