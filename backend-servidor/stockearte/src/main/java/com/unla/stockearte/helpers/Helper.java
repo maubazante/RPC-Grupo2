4b40e2162a0e16dfdc0c56ca9e72dfe0c034151d
Sergio Robles <mpthrash@gmail.com>
2024-10-26 19:29:39 -0300

    Update Helper.java
    + Agrega generarPDF

--- backend-servidor/stockearte/src/main/java/com/unla/stockearte/helpers/Helper.java
+++ backend-servidor/stockearte/src/main/java/com/unla/stockearte/helpers/Helper.java
@@ -1,7 +1,15 @@
 package com.unla.stockearte.helpers;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.security.SecureRandom;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -27,4 +35,24 @@ public class Helper {
 			super(message);
 		}
 	}
+
+	public static byte[] generarPDF(String contenido) throws IOException {
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		PDDocument document = new PDDocument();
+		PDPage page = new PDPage(PDRectangle.A4);
+		document.addPage(page);
+
+		PDPageContentStream contentStream = new PDPageContentStream(document, page);
+		contentStream.beginText();
+		contentStream.setFont(new PDType1Font(Standard14Fonts.FontName.HELVETICA), 14);
+		contentStream.newLineAtOffset(25, 700);
+		contentStream.showText(contenido);
+		contentStream.endText();
+		contentStream.close();
+
+		document.save(outputStream);
+		document.close();
+		return outputStream.toByteArray();
+	}
+
 }
